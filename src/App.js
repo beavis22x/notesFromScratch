d58e8e0fe39042377bd2b71eaf46e111a96c36b8
unknown <73778867+beavis22x@users.noreply.github.com>
2021-10-05 19:05:44 +0300

    Init html layout SPA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header className="header">
+                <img src="https://clck.ru/XzD4Y" className="header__logo"></img>
+                <nav className="nav__header">
+                    <div className="header__link">
+                        5 notes
+                    </div>
+                    <div className="header__link">
+                        10 notes
+                    </div>
+                    <div className="header__link">
+                        15 notes
+                    </div>
+                </nav>
+                <div className="header__burger">
+                    <span></span>
+                </div>
+            </header>
+            <main className="main">
+                <section className="note__items">
+                    <div className="note__item color3">
+                        HHHAAAAAAYYY
+                    </div>
+                    <div className="note__item color1">
+                        HHHAAAAAAYYY
+                    </div>
+                    <div className="note__item color2">
+                        HHHAAAAAAYYY
+                    </div>
+                    <div className="note__item color3">
+                        HHHAAAAAAYYY
+                    </div>
+                    <div className="note__item color4">
+                        HHHAAAAAAYYY
+                    </div>
+                    <div className="note__item color2">
+                        HHHAAAAAAYYY
+                    </div>
+                </section>
+            </main>
+            <footer className="footer">
+                <div>Copy, 2021</div>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
